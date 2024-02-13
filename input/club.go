fc47621f272dcba09afb928e96cd2c09152d14dd
shinohara320 <daudmanuwu@gmail.com>
2024-02-13 12:56:13 +0900

    add validate club name and input

--- input/club.go
+++ input/club.go
@@ -1,3 +1,5 @@
+// input/inputclub.go
+
 package input
 
 import (
@@ -5,12 +7,24 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/shinohara320/aptiv-klasemen/model"
 )
 
+var existingClubs []model.Club
+
 func InputClub() ([]model.Club, error) {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	clubExists := func(newClub model.Club) bool {
+		for _, club := range existingClubs {
+			if club.Name == newClub.Name && club.Kota == newClub.Kota {
+				return true
+			}
+		}
+		return false
+	}
 
 	fmt.Print("Masukkan jumlah klub yang akan dimasukkan: ")
 	scanner.Scan()
@@ -21,9 +35,9 @@ func InputClub() ([]model.Club, error) {
 		return nil, err
 	}
 
-	clubs := make([]model.Club, numClubs)
+	clubs := make([]model.Club, 0)
 
-	for i := 0; i < numClubs; i++ {
+	for i := 0; i < numClubs; {
 		fmt.Printf("\nInput klub #%d:\n", i+1)
 
 		fmt.Print("Nama klub: ")
@@ -34,7 +48,25 @@ func InputClub() ([]model.Club, error) {
 		scanner.Scan()
 		clubCity := scanner.Text()
 
-		clubs[i] = model.Club{Name: clubName, Kota: clubCity}
+		newClub := model.Club{Name: clubName, Kota: clubCity}
+
+		if !clubExists(newClub) {
+			clubs = append(clubs, newClub)
+			existingClubs = append(existingClubs, newClub)
+			fmt.Printf("Klub %d: %s dari kota %s\n", i+1, newClub.Name, newClub.Kota)
+			i++
+		} else {
+			fmt.Printf("Klub %d: %s dari kota %s sudah ada\n", i+1, newClub.Name, newClub.Kota)
+		}
+	}
+
+	fmt.Print("Apakah inputan sudah benar? [y/n]: ")
+	scanner.Scan()
+	response := strings.ToLower(scanner.Text())
+	if response != "y" {
+		fmt.Println("Inputan tidak benar. Ulangi input.")
+		existingClubs = nil
+		return InputClub()
 	}
 
 	return clubs, nil
